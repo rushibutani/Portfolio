1b0e74abe3c640f1d328cc18e7fd1c9ead86e84c
rushibutani <rushibutani@gmail.com>
2023-09-05 19:42:00 +0530

    update

--- src/CommoonComponents/Scroll/ScrollToTopButton.jsx
+++ src/CommoonComponents/Scroll/ScrollToTopButton.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { BsFillArrowUpSquareFill } from "react-icons/bs";
 
 const ScrollToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -27,10 +26,19 @@ const ScrollToTopButton = () => {
   }, []);
 
   return (
-    <BsFillArrowUpSquareFill
+    <div
       className={`scroll-to-top-button ${isVisible ? "visible" : ""}`}
       onClick={scrollToTop}
-    />
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        data-name="Layer 1"
+        viewBox="0 0 32 32"
+        id="up-arrow"
+      >
+        <path d="M26 22a2 2 0 0 1-1.41-.59L16 12.83l-8.59 8.58a2 2 0 0 1-2.82-2.82l10-10a2 2 0 0 1 2.82 0l10 10A2 2 0 0 1 26 22Z"></path>
+      </svg>
+    </div>
   );
 };
 
